d8a37bc6bad33db588443f962af2a5a9cb248d48
Raviraj39 <rravirajbujawade@gmail.com>
2026-01-29 20:04:06 +0530

    03 Episode

--- getting_Ready/App.js
+++ getting_Ready/App.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+//React Element
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello, React!"
 );
 
+// JSX => react element =>createElement() => HTML element (rendered in the browser)
+
+// const jsxheading = <h1 id="heading">Hello, React with JSX!</h1>;
+
+const Title = () => (
+  <h1 id="heading">Hello, React with JSX!</h1>
+);
+// React functional Component
+const MyComponent = () => (
+  <div>
+   <Title />
+   <h1>Hello, React functional component!  </h1>;
+   </div>
+);
+
+//component composition
+// using a component inside another component
+const MyComponent2 = () => (
+  <div>
+    {Title()}
+   <Title />
+   <Title></Title>
+   <h1>Hello, React functional component!  </h1>;
+   </div>
+);
+//{Title()}
+//  <Title />
+//  <Title></Title>
+// this all are same 
+
+
 const root = createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<MyComponent2 />);
